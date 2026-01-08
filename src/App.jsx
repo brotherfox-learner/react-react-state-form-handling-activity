41b63a673468ea5bb9a258066006d0b72766cb14
Nit <nitithon.taksaphan@gmail.com>
2026-01-08 13:31:32 +0700

    feat: implement email validation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,39 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleInput = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleValidate = (event) => {
+    event.preventDefault();
+    setEmail(text);
+    setText("");
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    setIsValid(regex.test(text));
+  };
+
   return (
     <div className="App">
       <h1>Email Validation App</h1>
-      <form className="email-container">
-        <h2 style={{ color: "red" }}>ผลลัพธ์จะมาแทนที่ตรงนี้</h2>
-        <input type="text" placeholder="Enter your email" />
+      <form className="email-container" onSubmit={handleValidate}>
+        {!email ? (
+          <h2 style={{ color: "white" }}>Enter Your email</h2>
+        ) : isValid ? (
+          <h2 style={{ color: "green" }}>✅ {email}</h2>
+        ) : (
+          <h2 style={{ color: "red" }}>❌ Wrong Pattern</h2>
+        )}
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={text}
+          onChange={handleInput}
+        />
         <div className="buttons">
           <button type="submit">Validate</button>
         </div>
