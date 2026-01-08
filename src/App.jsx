d2867f88745a6047fa6d7b28603fc53f47435c99
Nit <nitithon.taksaphan@gmail.com>
2026-01-08 11:01:57 +0700

    feat: implement greeting input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+  const [textInput, setTextInput] = useState("");
+  const handleInput = (event) => {
+    setGreeting(event.target.value);
+  };
+  const handleAdd = () => {
+    setTextInput(greeting);
+    setGreeting("");
+  };
+  
   return (
     <div className="App">
       <h1>Simple Greeting Message App 2</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
-        <input type="text" />
+        <h2>{textInput}</h2>
+        <input type="text" onChange={handleInput} value={greeting} />
         <div className="buttons">
-          <button>Add</button>
+          <button onClick={handleAdd}>Add</button>
         </div>
       </div>
     </div>
